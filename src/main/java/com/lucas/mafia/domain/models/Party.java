197c4cbde855384eea5ba0db14eeb3a9934ea9be
Lucas <lugab037@gmail.com>
2024-11-10 17:02:55 -0300

    feat: create Party entity with player management and game state logic

---
+++ src/main/java/com/lucas/mafia/domain/models/Party.java
@@ -0,0 +1,68 @@
+package com.lucas.mafia.domain.models;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.lucas.mafia.enums.GameStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Party {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private int changes;
+    private ArrayList<Player> players;
+    private GameStatus status;
+
+    public Party() {}
+
+    public Party(String leaderName) {
+        players = new ArrayList<>();
+        players.add(new Player(leaderName, true));
+
+        status = GameStatus.LOBBY;
+    }
+
+    public void addPlayer(String name) {
+        changes++;
+        players.add(new Player(name));
+    }
+
+    public void removePlayer(UUID id) {
+        changes++;
+        players.removeIf(player -> player.getId().equals(id));
+
+        if (!players.get(0).isLeader())
+            players.get(0).setLeader(true);
+    }
+
+    public void playerReady(UUID id) {
+        changes++;
+        int playersReady = 0;
+
+        for (Player player : players) {
+            if (player.getId().equals(id))
+                player.setReady(true);
+
+            if (player.isReady())
+                playersReady++;
+        }
+
+        if (playersReady == players.size())
+            status = GameStatus.ASSASSIN_TURN;
+    }
+
+    public void startGame() {
+        changes++;
+        status = GameStatus.ASSASSIN_TURN;
+    }
+}
